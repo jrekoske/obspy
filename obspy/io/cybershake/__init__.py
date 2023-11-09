abdf3b229ef51b35375aca9b247c4e29204b638a
John Rekoske <john.rekoske@gmail.com>
2023-11-08 21:31:44 -0800

    Temporary commit.

---
+++ obspy/io/cybershake/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+obspy.io.cybershake - CyberShake read support for ObsPy
+===========================================================
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (https://www.gnu.org/copyleft/lesser.html)
+"""
---
+++ obspy/io/cybershake/core.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Support for reading CyberShake seismograms in ObsPy.
+"""
+
+import numpy as np
+
+def read(): 
+    # code for reading seismograms
+    return
